071ef19a87c999de697f41117affc921e8d86cbf
Igor Santos <igorsantos07@gmail.com>
2011-01-12 17:38:56 -0200

    adding accounts file

---
+++ accounts.rb
@@ -0,0 +1,12 @@
+$accounts = [
+  {
+    :username => 'TrendsRJ',
+    :password => 'ttrjcs2011',
+    :woeid    => 455825
+  },
+  {
+    :username => 'TrendsSP',
+    :password => 'ttspcs2011',
+    :woeid    => 455827
+  }
+]
