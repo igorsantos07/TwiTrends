ae0ccf4c6228694e753e232cb10afc905cb9fb4e
Igor Santos <igorsantos07@gmail.com>
2011-01-12 23:48:53 -0200

    Moving accounts data to a YAML hidden file called 'accounts.yaml', and finishing tweeting code

--- tt.rb
+++ tt.rb
@@ -1,19 +1,36 @@
 require 'twitter'
-require 'accounts'
+$debug = false
 
-$accounts.each do |acc|
-  puts "Getting Trending Topics for WOEID #{acc[:woeid]} and tweeting to #{acc[:username]}..."
+$format = '[%s] %s' # [time] Trending topics
+
+now  = Time.now
+time = ('%2d'%now.hour)+'h'+('%2d'%now.min)
+
+YAML::load_file('accounts.yaml').each_pair do |title, acc|
+  print "Getting Trending Topics and tweeting to #{title}..."
 
   Twitter.configure do |c|
-    c.consumer_key       = acc[:consumer_key]
-    c.consumer_secret    = acc[:consumer_secret]
-    c.oauth_token        = acc[:oauth_key]
-    c.oauth_token_secret = acc[:oauth_secret]
+    c.consumer_key       = acc['consumer_key']
+    c.consumer_secret    = acc['consumer_secret']
+    c.oauth_token        = acc['oauth_key']
+    c.oauth_token_secret = acc['oauth_secret']
   end
 
   twitter = Twitter::Client.new
 
-  trends = twitter.local_trends(acc[:woeid])
+  trends = twitter.local_trends(acc['woeid'])
   puts trends.inspect
-  puts ''
+
+	[
+		$format % [time, trends[0..4].join(' | ')],
+		$format % [time, trends[5..9].join(' | ')]
+	].each do |tweet|
+		if $debug
+			puts "Tweet >> "+tweet
+		else
+			twitter.update tweet
+		end
+	end
+
+	puts ''
 end
