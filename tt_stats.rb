3d195bc43baf02ea898fe67073b253162e6cfaa2
Igor Santos <igorsantos07@gmail.com>
2011-02-03 22:18:22 -0200

    Fix for color insert sql

--- tt_stats.rb
+++ tt_stats.rb
@@ -17,7 +17,7 @@ db = SQLite3::Database.new((File.exists? 'stats.db')? 'stats.db' : $LOAD_PATH[0]
 yaml_file = (File.exists? 'accounts.yaml')? 'accounts.yaml' : $LOAD_PATH[0]+'/accounts.yaml'
 YAML::load_file(yaml_file).each_pair do |account,data|
   acc_id = if db.get_first_value('SELECT COUNT(*) FROM accounts WHERE name=?', account) == '0'
-    db.execute 'INSERT INTO accounts (name,color) VALUES (?,?)', account, data['color']
+    db.execute 'INSERT INTO accounts (name,color) VALUES (?,?)', account, data['account']['color']
     db.last_insert_row_id
   else
     db.execute 'SELECT id FROM accounts WHERE name=?', account
